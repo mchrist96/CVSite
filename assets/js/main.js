e4c5bd7b1265d5ad145e0fb36a4b243b2b154780
mchrist96 <92491074+mchrist96@users.noreply.github.com>
2026-08-14 14:51:45 -0600

    Load shared styling for project pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,106 +12,83 @@
 		$footer = $('#footer'),
 		$main = $('#main'),
 		settings = {
-
-			// Parallax background effect?
 				parallax: true,
-
-			// Parallax factor (lower = more intense, higher = less intense).
 				parallaxFactor: 20
-
 		};
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:  [ '1281px',  '1800px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ '481px',   '736px'  ],
-			xsmall:  [ null,      '480px'  ],
-		});
+	breakpoints({
+		xlarge:  [ '1281px',  '1800px' ],
+		large:   [ '981px',   '1280px' ],
+		medium:  [ '737px',   '980px'  ],
+		small:   [ '481px',   '736px'  ],
+		xsmall:  [ null,      '480px'  ],
+	});
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+	$window.on('load', function() {
+		window.setTimeout(function() {
+			$body.removeClass('is-preload');
+		}, 100);
+	});
 
-	// Touch?
-		if (browser.mobile) {
+	if (browser.mobile) {
+		$body.addClass('is-touch');
+		window.setTimeout(function() {
+			$window.scrollTop($window.scrollTop() + 1);
+		}, 0);
+	}
 
-			// Turn on touch mode.
-				$body.addClass('is-touch');
+	breakpoints.on('<=medium', function() {
+		$footer.insertAfter($main);
+	});
 
-			// Height fix (mostly for iOS).
-				window.setTimeout(function() {
-					$window.scrollTop($window.scrollTop() + 1);
-				}, 0);
+	breakpoints.on('>medium', function() {
+		$footer.appendTo($header);
+	});
 
-		}
+	if (browser.name == 'ie' || browser.mobile)
+		settings.parallax = false;
 
-	// Footer.
+	if (settings.parallax) {
 		breakpoints.on('<=medium', function() {
-			$footer.insertAfter($main);
+			$window.off('scroll.strata_parallax');
+			$header.css('background-position', '');
 		});
 
 		breakpoints.on('>medium', function() {
-			$footer.appendTo($header);
+			$header.css('background-position', 'left 0px');
+			$window.on('scroll.strata_parallax', function() {
+				$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
+			});
 		});
 
-	// Header.
+		$window.on('load', function() {
+			$window.triggerHandler('scroll');
+		});
+	}
 
-		// Parallax background.
+	$window.on('load', function() {
+		$('#two').poptrox({
+			caption: function($a) { return $a.next('h3').text(); },
+			overlayColor: '#2c2c2c',
+			overlayOpacity: 0.85,
+			popupCloserText: '',
+			popupLoaderText: '',
+			selector: '.work-item a.image',
+			usePopupCaption: true,
+			usePopupDefaultStyling: false,
+			usePopupEasyClose: false,
+			usePopupNav: true,
+			windowMargin: (breakpoints.active('<=small') ? 0 : 50)
+		});
+	});
 
-			// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
-				if (browser.name == 'ie'
-				||	browser.mobile)
-					settings.parallax = false;
-
-			if (settings.parallax) {
-
-				breakpoints.on('<=medium', function() {
-
-					$window.off('scroll.strata_parallax');
-					$header.css('background-position', '');
-
-				});
-
-				breakpoints.on('>medium', function() {
-
-					$header.css('background-position', 'left 0px');
-
-					$window.on('scroll.strata_parallax', function() {
-						$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
-					});
-
-				});
-
-				$window.on('load', function() {
-					$window.triggerHandler('scroll');
-				});
-
-			}
-
-	// Main Sections: Two.
-
-		// Lightbox gallery.
-			$window.on('load', function() {
-
-				$('#two').poptrox({
-					caption: function($a) { return $a.next('h3').text(); },
-					overlayColor: '#2c2c2c',
-					overlayOpacity: 0.85,
-					popupCloserText: '',
-					popupLoaderText: '',
-					selector: '.work-item a.image',
-					usePopupCaption: true,
-					usePopupDefaultStyling: false,
-					usePopupEasyClose: false,
-					usePopupNav: true,
-					windowMargin: (breakpoints.active('<=small') ? 0 : 50)
-				});
-
-			});
+	// Project pages use the same visual language as the resume and portfolio.
+	if (document.getElementById('project-content')) {
+		$body.addClass('project-page');
+		var stylesheet = document.createElement('link');
+		stylesheet.rel = 'stylesheet';
+		stylesheet.href = '../assets/css/project-enhancements.css';
+		document.head.appendChild(stylesheet);
+	}
 
 })(jQuery);
